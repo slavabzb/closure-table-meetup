7c6d770c9cce9c20db4faacc22d331a0b9356b92
Vyacheslav Bezborodov <vyacheslav.bezborodov@antidasoftware.com>
2018-04-22 15:59:38 +0500

    basic structure

---
+++ src/closure_table/cli.py
@@ -0,0 +1,18 @@
+import click
+from aiohttp.web import (
+    Application,
+    run_app,
+)
+from closure_table.setup import setup_app
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def serve():
+    app = Application()
+    setup_app(app)
+    run_app(app)
---
+++ src/closure_table/settings.py
@@ -0,0 +1,5 @@
+DATABASE = 'postgresql://closureuser:closurepass@localhost/closuredb'
+
+JWT_SECRET = 'secret'
+JWT_ALGORITHM = 'HS256'
+JWT_EXP_DELTA_SECONDS = 60 * 5
